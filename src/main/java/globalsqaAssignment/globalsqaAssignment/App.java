a8953809a40f422af7ff3a274bbae86cd80a53af
Your Name <githubtest2024@gmail.com>
2025-02-08 15:08:41 +0530

    Home page test

---
+++ src/main/java/globalsqaAssignment/globalsqaAssignment/App.java
@@ -0,0 +1,10 @@
+package globalsqaAssignment.globalsqaAssignment;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
